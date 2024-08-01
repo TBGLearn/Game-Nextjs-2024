0e8907e57debae5883e5a5058eccbbbe8f8eb2fd
duy59 <129411107+duy59@users.noreply.github.com>
2024-08-01 09:24:48 +0700

    update 1/8

--- src/pages/games/[page].js
+++ src/pages/games/[page].js
@@ -1,96 +1,96 @@
-import React from 'react';
-import GameList from '../../components/Games/index';
-import Head from 'next/head';
-import { useRouter } from 'next/router';
+// import React from 'react';
+// import GameList from '../../components/Games/index';
+// import Head from 'next/head';
+// import { useRouter } from 'next/router';
 
-export const config = {
-  unstable_runtimeJS: false
-};
+// export const config = {
+//   unstable_runtimeJS: false
+// };
 
 
-async function getData(pageActive, limit) {
-  const res = await fetch(`https://game.tbg95.com/api/game-list?page=${pageActive}&perPage=${limit}`);
-  return res.json();
-}
+// async function getData(pageActive, limit) {
+//   const res = await fetch(`https://game.tbg95.com/api/game-list?page=${pageActive}&perPage=${limit}`);
+//   return res.json();
+// }
 
-export async function getStaticProps({ params }) {
-  const pageActive = parseInt(params.page, 10) - 1;
-  const limit = 30;
-  const result = await getData(pageActive, limit);
+// export async function getStaticProps({ params }) {
+//   const pageActive = parseInt(params.page, 10) - 1;
+//   const limit = 30;
+//   const result = await getData(pageActive, limit);
 
-  return {
-    props: {
-      initialData: result.data || [],
-      initialCategories: result.categories || [],
-      initialPageActive: pageActive,
-      initialPageQuantity: 10
-    }
-  };
-}
+//   return {
+//     props: {
+//       initialData: result.data || [],
+//       initialCategories: result.categories || [],
+//       initialPageActive: pageActive,
+//       initialPageQuantity: 10
+//     }
+//   };
+// }
 
-export async function getStaticPaths() {
-  const res = await fetch(`https://game.tbg95.com/api/game-list?page=0&perPage=1`);
-  const result = await res.json();
-  // const totalPages = Math.ceil(result.totalPage || 0);
-  const totalPages = 10;
+// export async function getStaticPaths() {
+//   const res = await fetch(`https://game.tbg95.com/api/game-list?page=0&perPage=1`);
+//   const result = await res.json();
+//   // const totalPages = Math.ceil(result.totalPage || 0);
+//   const totalPages = 10;
 
-  const paths = Array.from({ length: totalPages }, (_, i) => ({
-    params: { page: (i + 1).toString() },
-  }));
+//   const paths = Array.from({ length: totalPages }, (_, i) => ({
+//     params: { page: (i + 1).toString() },
+//   }));
 
-  return { paths, fallback: false };
-}
+//   return { paths, fallback: false };
+// }
 
-export default function Games({ initialData, initialCategories, initialPageActive, initialPageQuantity }) {
-  const router = useRouter();
-  const [data, setData] = React.useState(initialData);
-  const [categories, setCategories] = React.useState(initialCategories);
-  const [pageActive, setPageActive] = React.useState(initialPageActive);
-  const [pageQuantity, setPageQuantity] = React.useState(initialPageQuantity);
+// export default function Games({ initialData, initialCategories, initialPageActive, initialPageQuantity }) {
+//   const router = useRouter();
+//   const [data, setData] = React.useState(initialData);
+//   const [categories, setCategories] = React.useState(initialCategories);
+//   const [pageActive, setPageActive] = React.useState(initialPageActive);
+//   const [pageQuantity, setPageQuantity] = React.useState(initialPageQuantity);
 
-  React.useEffect(() => {
-    const fetchData = async () => {
-      const result = await getData(pageActive, 30);
-      setData(result.data || []);
-    };
+//   React.useEffect(() => {
+//     const fetchData = async () => {
+//       const result = await getData(pageActive, 30);
+//       setData(result.data || []);
+//     };
 
-    fetchData();
-  }, [pageActive]);
+//     fetchData();
+//   }, [pageActive]);
 
-  const handleClickPage = async (pageIndex) => {
-    setPageActive(pageIndex);
-    router.push(`/games/${pageIndex + 1}`);
-  };
+//   const handleClickPage = async (pageIndex) => {
+//     setPageActive(pageIndex);
+//     router.push(`/games/${pageIndex + 1}`);
+//   };
 
-  const handlePreviousPage = () => {
-    if (pageActive > 0) {
-      setPageActive(pageActive - 1);
-      router.push(`/games/${pageActive}`);
-    }
-  };
+//   const handlePreviousPage = () => {
+//     if (pageActive > 0) {
+//       setPageActive(pageActive - 1);
+//       router.push(`/games/${pageActive}`);
+//     }
+//   };
 
-  const handleNextPage = () => {
-    if (pageActive < pageQuantity - 1) {
-      setPageActive(pageActive + 1);
-      router.push(`/games/${pageActive + 2}`);
-    }
-  };
+//   const handleNextPage = () => {
+//     if (pageActive < pageQuantity - 1) {
+//       setPageActive(pageActive + 1);
+//       router.push(`/games/${pageActive + 2}`);
+//     }
+//   };
 
-  return (
-    <>
-      <Head>
-        <title>All OF GAME</title>
-        <meta name="description" content="Khám phá tất cả các trò chơi trên trang của chúng tôi với bộ sưu tập phong phú và đa dạng. Từ trò chơi hành động, phiêu lưu, đến các trò chơi trí tuệ, chúng tôi có mọi thứ bạn cần để giải trí và thư giãn." />
-      </Head>
-      <GameList
-        data={data}
-        categories={categories}
-        pageActive={pageActive}
-        pageQuantity={pageQuantity}
-        onClickPage={handleClickPage}
-        onPreviousPage={handlePreviousPage}
-        onNextPage={handleNextPage}
-      />
-    </>
-  );
-}
+//   return (
+//     <>
+//       <Head>
+//         <title>All OF GAME</title>
+//         <meta name="description" content="Khám phá tất cả các trò chơi trên trang của chúng tôi với bộ sưu tập phong phú và đa dạng. Từ trò chơi hành động, phiêu lưu, đến các trò chơi trí tuệ, chúng tôi có mọi thứ bạn cần để giải trí và thư giãn." />
+//       </Head>
+//       <GameList
+//         data={data}
+//         categories={categories}
+//         pageActive={pageActive}
+//         pageQuantity={pageQuantity}
+//         onClickPage={handleClickPage}
+//         onPreviousPage={handlePreviousPage}
+//         onNextPage={handleNextPage}
+//       />
+//     </>
+//   );
+// }
--- src/pages/games/index.js
+++ src/pages/games/index.js
@@ -1,15 +1,89 @@
 // pages/games/index.js
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
+import GameList from '../../components/Games/index';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 
-const Home = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push('/games/1');
-  }, [router]);
-
-  return null;
+export const config = {
+  unstable_runtimeJS: false
 };
 
-export default Home;
+async function getData(pageActive, limit) {
+  const res = await fetch(`https://game.tbg95.com/api/game-list?page=${pageActive}&perPage=${limit}`);
+  return res.json();
+}
+
+export async function getStaticProps() {
+  const pageActive = 0; // Trang đầu tiên
+  const limit = 30;
+  const result = await getData(pageActive, limit);
+
+  return {
+    props: {
+      initialData: result.data || [],
+      initialCategories: result.categories || [],
+      initialPageQuantity: 10
+    }
+  };
+}
+
+export default function Games({ initialData, initialCategories, initialPageQuantity }) {
+  const router = useRouter();
+  const [data, setData] = React.useState(initialData);
+  const [categories, setCategories] = React.useState(initialCategories);
+  const [pageActive, setPageActive] = React.useState(0);
+  const [pageQuantity, setPageQuantity] = React.useState(initialPageQuantity);
+
+  useEffect(() => {
+    const { page } = router.query;
+    if (page) {
+      setPageActive(parseInt(page, 10) - 1);
+    }
+  }, [router.query]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getData(pageActive, 30);
+      setData(result.data || []);
+    };
+
+    fetchData();
+  }, [pageActive]);
+
+  const handleClickPage = async (pageIndex) => {
+    setPageActive(pageIndex);
+    router.push(`/games?page=${pageIndex + 1}`, undefined, { shallow: true });
+  };
+
+  const handlePreviousPage = () => {
+    if (pageActive > 0) {
+      setPageActive(pageActive - 1);
+      router.push(`/games?page=${pageActive}`, undefined, { shallow: true });
+    }
+  };
+
+  const handleNextPage = () => {
+    if (pageActive < pageQuantity - 1) {
+      setPageActive(pageActive + 1);
+      router.push(`/games?page=${pageActive + 2}`, undefined, { shallow: true });
+    }
+  };
+
+  return (
+    <>
+      <Head>
+        <title>All OF GAME</title>
+        <meta name="description" content="Khám phá tất cả các trò chơi trên trang của chúng tôi với bộ sưu tập phong phú và đa dạng. Từ trò chơi hành động, phiêu lưu, đến các trò chơi trí tuệ, chúng tôi có mọi thứ bạn cần để giải trí và thư giãn." />
+      </Head>
+      <GameList
+        data={data}
+        categories={categories}
+        pageActive={pageActive}
+        pageQuantity={pageQuantity}
+        onClickPage={handleClickPage}
+        onPreviousPage={handlePreviousPage}
+        onNextPage={handleNextPage}
+      />
+    </>
+  );
+}
